e27e3648167963332edc016e65695579f1a27c9a
fossilrain@org <1024254883@qq.com>
2017-03-02 11:27:48 +0800

    测试数组参数

--- src/cn/com/jna/test/HelloWorld.java
+++ src/cn/com/jna/test/HelloWorld.java
@@ -1,5 +1,8 @@
 package cn.com.jna.test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
@@ -16,10 +19,18 @@ public class HelloWorld {
     }
  
     public static void main(String[] args) {
-        CLibrary.INSTANCE.printf("Hello, World/n");
+        /*CLibrary.INSTANCE.printf("Hello, World/n");
         for (int i=0;i < args.length;i++) {
             CLibrary.INSTANCE.printf("Argument %d: %s/n", i, args[i]);
-        }
+        }*/
+    	String keywords = "ÎÒ´òµÄ¾ÍÊÇÂÒ";
+    	Pattern pattern = Pattern.compile("[\\p{P}0-9a-zA-Z\u4e00-\u9fa5]*");
+	    Matcher isNormal = pattern.matcher(keywords);
+	    if(isNormal.matches()){
+	    	System.out.println("不是乱码："+keywords);
+	    }else{
+	    	System.out.println("是乱码："+keywords);
+	    }
     }
 }
  
